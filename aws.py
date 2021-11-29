34124d0d1a0e2b1fe9d73d17baf3d64f843994f7
Cesar Reynosa <creynosa@protonmail.com>
2021-11-29 11:52:38 -0600

    Ok, for real, fixed the issue with env variables.

--- aws.py
+++ aws.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 ACCESS_KEY = os.environ["s3_access_key"]
-SECRET_KEY = os.environ["s3_secret_access-key"]
+SECRET_KEY = os.environ["s3_secret_access_key"]
 s3 = boto3.client("s3", aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
